6613551f13ec6ad19eea6ce003aeac66e406276a
Reno Sorensen <renojames19@gmail.com>
2019-06-12 04:02:11 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,28 @@ class UsersController < ApplicationController
     binding.pry
   end
 
+def entered_username?
+  if !!params["username"] && params["username"] != ""
+    true
+  else
+    false
+  end
+end
+
+def entered_email?
+  if !!params["email"] && params["email"] != ""
+    true
+  else
+    false
+  end
+end
+
+def entered_password?
+  if !!params["password"] && params["password"] != ""
+    true
+  else
+    false
+  end
+end
+
 end
