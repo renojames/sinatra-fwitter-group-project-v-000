4103e579495df668811f1f3afcee9e6816d5f70d
Reno Sorensen <renojames19@gmail.com>
2019-06-18 01:58:24 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,9 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    @user = User.find(session[:user_id])
 
     if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
       erb :"/tweets/tweets"
     else
       redirect '/login'
