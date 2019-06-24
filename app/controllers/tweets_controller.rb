0224fa41297e454c47d97a75613ca58481d31b98
Reno Sorensen <renojames19@gmail.com>
2019-06-24 00:33:31 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,32 +10,41 @@ class TweetsController < ApplicationController
     end
   end
 
- get '/tweets/new' do
-   if Helpers.is_logged_in?(session)
-     erb :"/tweets/new"
-   else
-     redirect '/login'
-   end
- end
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :"/tweets/new"
+    else
+      redirect '/login'
+    end
+  end
 
- post '/tweets' do
-   if !!params["content"] && params["content"] != ""
-     @tweet = Tweet.new(content: params["content"])
-     @tweet.user = Helpers.current_user(session)
-     @tweet.save
-   else
-     redirect "/tweets/new"
-   end
- end
+  post '/tweets' do
+    if !!params["content"] && params["content"] != ""
+      @tweet = Tweet.new(content: params["content"])
+      @tweet.user = Helpers.current_user(session)
+      @tweet.save
+    else
+      redirect "/tweets/new"
+    end
+  end
 
- get '/tweets/:id' do
-   if Helpers.is_logged_in?(session)
-     @tweet = Tweet.find(params[:id])
-     erb :"/tweets/show_tweet"
-   else
-     redirect "/login"
-   end
- end
+  get '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/show_tweet"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if Helpers.is_logged_in?(session) && Tweet.find(params[:id]).user == Helpers.current_user(session)
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+      redirect "/login"
+    end
+  end
 
 
 end
