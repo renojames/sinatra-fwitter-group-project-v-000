384bf70d7d294e1aa85bbee9850c041acbc7f83b
Reno Sorensen <renojames19@gmail.com>
2019-06-24 00:05:33 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,6 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-
     if Helpers.is_logged_in?(session)
       @user = User.find(session[:user_id])
       @tweets = Tweet.all
@@ -10,5 +9,24 @@ class TweetsController < ApplicationController
       redirect '/login'
     end
   end
+
+ get '/tweets/new' do
+   if Helpers.is_logged_in?(session)
+     erb :"/tweets/new"
+   else
+     redirect '/login'
+   end
+ end
+
+ post '/tweets' do
+   if !!params["content"] && params["content"] != ""
+     @tweet = Tweet.new(content: params["content"])
+     @tweet.user = Helpers.current_user(session)
+     @tweet.save
+   else
+     redirect "/tweets/new"
+   end
+ end
+
 
 end
